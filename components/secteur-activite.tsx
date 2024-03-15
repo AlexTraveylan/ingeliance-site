166b09f42abc19c2a3c9baa7ec270f770cf3116b
TDEMARES <timothee.demares@ingeliance.com>
2024-03-15 10:39:53 +0100

    caroussel responsive

--- components/secteur-activite.tsx
+++ components/secteur-activite.tsx
@@ -1,6 +1,9 @@
+"use client"
+
 import { Card, CardContent, CardFooter } from "@/components/ui/card"
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
 import { navItems } from "@/lib/navBarContent"
+import Autoplay from "embla-carousel-autoplay"
 import Image from "next/image"
 import Link from "next/link"
 import { Button } from "./ui/button"
@@ -8,13 +11,25 @@ import { Button } from "./ui/button"
 export function SecteurActivite() {
   return (
     <div className="flex justify-center w-full py-5 bg-slate-200">
-      <Carousel className="w-full max-w-screen-xl">
-        <CarouselContent className="max-w-7xl">
+      <Carousel
+        className="w-full max-w-sm md:max-w-2xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7xl"
+        opts={{
+          align: "center",
+          loop: true,
+        }}
+        plugins={[
+          Autoplay({
+            delay: 3000,
+          }),
+        ]}
+      >
+        <h1 className=" font-bold text-4xl text-center py-5">{"Secteurs d'activité".toUpperCase()}</h1>
+        <CarouselContent className="-ml-2 md:-ml-4">
           {navItems["secteur"].map((secteurItem, index) => (
-            <CarouselItem key={`carousel_secteur_${index}`} className="pl-1 md:basis-1/2 lg:basis-1/3">
+            <CarouselItem key={`carousel_secteur_${index}`} className="pl-2 md:pl-4 basis-full md:basis-1/2 xl:basis-1/3">
               <div className="p-1">
                 <Card className="aspect-square items-center justify-center flex flex-col">
-                  <CardContent className="flex items-center justify-center py-3">
+                  <CardContent className="flex items-center justify-center mt-2">
                     {secteurItem?.img_url && <Image src={secteurItem.img_url} alt={secteurItem.title} width={500} height={350} />}
                   </CardContent>
                   <CardFooter className="flex flex-col gap-3">
